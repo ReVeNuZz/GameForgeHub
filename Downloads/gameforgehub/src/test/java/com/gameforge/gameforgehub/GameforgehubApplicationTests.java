a73b6c9611bb81aa078e540efc2e57f6ae8db03b
Yadiel <estradanunezyadiel@gmail.com>
2026-03-21 18:48:14 -0400

    Initial commit

---
+++ Downloads/gameforgehub/src/test/java/com/gameforge/gameforgehub/GameforgehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameforge.gameforgehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameforgehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
